eb6b590a57c93ae3ece3cc8050c3f20fae9c54a0
Bong1hong <bong1hongyeah@gmail.com>
2019-04-11 11:39:43 +0800

    added some code in facebook login

--- login.php
+++ login.php
@@ -112,32 +112,35 @@
     $helper = $fb->getRedirectLoginHelper();
     $login_url = $helper->getLoginUrl("http://localhost/salon-management-software/login.php",["email"]);
     
-    try {
-        $accessToken = $helper->getAccessToken();
-        if (isset($accessToken)) {
-            // Problem Here
-            $_SESSION["access_token"] = (string)$accessToken;
-            
-            if ($_SESSION["access_token"]) {
-                try {
-                    //$info = explode('=', $accessToken);
-                    //$fb->setDefaultAccessToken($info[1]);
-                    $fb->setDefaultAccessToken($_SESSION["access_token"]);
-                    $res = $fb->get("/me?locale=en_US&fields=name,email", $_SESSION["access_token"]);
-                    $user = $res->getGraphUser();
-                    $_SESSION["name"] = $user->getField("name");
-                    $_SESSION["email"] = $user->getField("email");
-                    header("Location: index.php");
-                } catch (Exception $exc) {
-                    echo $exc->getTraceAsString();
+    if (!isset($_SESSION['access_token'])){
+        try {
+
+            $accessToken = $helper->getAccessToken();
+            if (isset($accessToken)) {
+                // Problem Here
+                $_SESSION["access_token"] = (string)$accessToken;
+
+                if ($_SESSION["access_token"]) {
+                    try {
+                        //$info = explode('=', $accessToken);
+                        //$fb->setDefaultAccessToken($info[1]);
+                        $fb->setDefaultAccessToken($_SESSION["access_token"]);
+                        $res = $fb->get("/me?locale=en_US&fields=name,email", $_SESSION["access_token"]);
+                        $user = $res->getGraphUser();
+                        $_SESSION["name"] = $user->getField("name");
+                        $_SESSION["email"] = $user->getField("email");
+                        header("Location: index.php");
+                    } catch (Exception $exc) {
+                        echo $exc->getTraceAsString();
+                    }
                 }
+
+                //header("Location: index.php");
+
             }
-            
-            //header("Location: index.php");
-            
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
         }
-    } catch (Exception $exc) {
-        echo $exc->getTraceAsString();
     }
     
 ?>
