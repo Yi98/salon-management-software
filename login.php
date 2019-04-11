42f8bd42974ba3ce2e126c30e5b300ffbc8deffe
Bong1hong <bong1hongyeah@gmail.com>
2019-04-11 12:20:51 +0800

    Fixes a bug occur in login.php where page can't be load due to unexpected end of file error

--- login.php
+++ login.php
@@ -100,20 +100,17 @@
 
 <!-- Facebook login php  -->
 <?php
-    require "vendor/autoload.php";
+     require "vendor/autoload.php";
 
     $fb = new Facebook\Facebook([
         "app_id" => "2199009383524440",
         "app_secret" => "20beaba1dc33f9db6a3a703979723015",
         "default_graph_version" => "v2.7"
-        
     ]);
     
     $helper = $fb->getRedirectLoginHelper();
     $login_url = $helper->getLoginUrl("http://localhost/salon-management-software/login.php",["email"]);
     
-<<<<<<< HEAD
-
     try {
         $accessToken = $helper->getAccessToken();
         if (isset($accessToken)) {
@@ -132,38 +129,16 @@
                     header("Location: index.php");
                 } catch (Exception $exc) {
                     echo $exc->getTraceAsString();
-=======
-    if (!isset($_SESSION['access_token'])){
-        try {
-
-            $accessToken = $helper->getAccessToken();
-            if (isset($accessToken)) {
-                // Problem Here
-                $_SESSION["access_token"] = (string)$accessToken;
-
-                if ($_SESSION["access_token"]) {
-                    try {
-                        //$info = explode('=', $accessToken);
-                        //$fb->setDefaultAccessToken($info[1]);
-                        $fb->setDefaultAccessToken($_SESSION["access_token"]);
-                        $res = $fb->get("/me?locale=en_US&fields=name,email", $_SESSION["access_token"]);
-                        $user = $res->getGraphUser();
-                        $_SESSION["name"] = $user->getField("name");
-                        $_SESSION["email"] = $user->getField("email");
-                        header("Location: index.php");
-                    } catch (Exception $exc) {
-                        echo $exc->getTraceAsString();
-                    }
->>>>>>> eb6b590a57c93ae3ece3cc8050c3f20fae9c54a0
                 }
-
-                //header("Location: index.php");
-
             }
-        } catch (Exception $exc) {
-            echo $exc->getTraceAsString();
+            
+            //header("Location: index.php");
+            
         }
+    } catch (Exception $exc) {
+        echo $exc->getTraceAsString();
     }
+
     
 ?>
 
